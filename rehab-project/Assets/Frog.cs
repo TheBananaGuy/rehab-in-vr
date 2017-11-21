53c7a7c17735682523eb9eb47dfbdea9da16e9de
Sam Mullen <our.denouement@gmail.com>
2017-11-21 13:16:57 +0100

    relocation; environment added;

---
+++ rehab-project/Assets/Frog.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Frog : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Transform endPoint;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setEndPoint(Transform t)
+    {
+        endPoint = t;
+        agent.destination = endPoint.position;
+        agent.isStopped = false;
+    }
+}
