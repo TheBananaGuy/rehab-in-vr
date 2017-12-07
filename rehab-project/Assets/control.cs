e240b8b044c36b24afdfd96a3b6604da0ed7872f
Sam Mullen <our.denouement@gmail.com>
2017-12-07 12:37:40 +0100

    particles initiated

---
+++ rehab-project/Assets/control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+	public Transform sparkle;
+
+	// Use this for initialization
+	void Start () {
+		sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Hand_Left") {
+			sparkle.GetComponent<ParticleSystem>().enableEmission = true;
+			StartCoroutine (stopSparkles());
+		}
+	}
+
+	IEnumerator stopSparkles()
+	{
+		yield return new WaitForSeconds (.4f);
+		sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
